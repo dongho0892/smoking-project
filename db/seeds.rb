28b5a1f5ad8b0362083c7b1e02638de1eccfe5a3
Dongho <dongho0892@daum.net>
2018-07-24 08:21:43 +0000

    seeds.rb 삭제

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,3 @@
 
 
 # rake import_csv:data
-Product.create(area_type: '단속구역', area_name: 'In Korea Territory', latitude: '37.24072264134755', longitude: '131.8672111341569')
-
-
-require 'csv'
-
-namespace :import_csv do 
-    task data: :environment do
-        #user.csv의 경로를 filename에 저장
-        CSV.foreach("app/assets/no_smoking_place.csv", :headers => true) do |row|
-            # p row
-            #users.csv파일의 각 행을 array 형태로 출력
-            Home.create!(row.to_hash)
-        end
-    end
-end
